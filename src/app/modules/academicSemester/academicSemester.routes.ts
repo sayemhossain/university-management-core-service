edf14f0346d8e2840fdbb385079e55c7394cac73
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2023-08-18 04:03:53 +0600

    zod is implement for academic semester

--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterController } from './academicSemester.controller';
+import { AcademicSemesterValidation } from './academicSemester.validation';
 const router = express.Router();
 
-router.post('/', AcademicSemesterController.insertIntoDB);
+router.post(
+  '/',
+  validateRequest(AcademicSemesterValidation.create),
+  AcademicSemesterController.insertIntoDB
+);
 
 export const AcademicSemesterRoutes = router;
